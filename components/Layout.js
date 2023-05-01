a05ba5b903f705da2cbe55c5778113b324df3c43
Andrew Bwogi <andrew.bwogi@gmail.com>
2023-04-30 23:42:37 -0700

    adding head and meta data via layout

---
+++ components/Layout.js
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+
+export default function Layout({ title, keywords, description, children }) {
+	return (
+		<div>
+			<Head>
+				<title>{title}</title>
+				<meta
+					name='description'
+					content={description}
+				/>
+				<meta
+					name='keywords'
+					content={keywords}
+				/>
+			</Head>
+
+			{children}
+		</div>
+	);
+}
+
+Layout.defaultProps = {
+	title: 'DJ Events | Find the hottest parties',
+	keywords: 'music, dj, events',
+	description: 'Find the latest DJ and other musical events',
+};
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function about() {
-	return <div>about</div>;
+	return (
+		<div>
+			<h1>About Page</h1>
+			<p>This is an app to find the latest dj and other musical events</p>
+			<p>Version 1.0.0</p>
+		</div>
+	);
 }
